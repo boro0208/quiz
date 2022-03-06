ddf35bc172de3c8167f433fc180b45d8e88f0d35
Boro Radojčić <bororadojcic1991@gmail.com>
2022-03-06 10:24:28 +0100

    leaderborad page

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from PyQt5 import QtWidgets
 from model.question import Question
 from model.user import User
 from py_ui.start_page import Ui_MainWindow
+from py_ui.leaderboard_page import Ui_MainWindow as Ui_leader
 from py_ui.main_page import Ui_Quiz as Ui_Main
 from db import db_helper as d
 import sys
 import requests
+import html
 
 questions = []
 
@@ -23,6 +25,19 @@ def call_api(level):
     question_data = response.json()["results"]
     for i in question_data:
         questions.append(Question(i['question'], i['correct_answer'], i['incorrect_answers']))
+
+
+class LeadersPage(QtWidgets.QMainWindow, Ui_leader):
+    def __init__(self, parent=None):
+        super(LeadersPage, self).__init__(parent)
+        self.setupUi(self)
+        self.newGame.clicked.connect(self.restart)
+
+    def restart(self):
+        questions.clear()
+        self.main_page = StartPage()
+        self.main_page.show()
+        self.close()
 
 
 class MainPage(QtWidgets.QMainWindow, Ui_Main):
@@ -41,15 +56,15 @@ class MainPage(QtWidgets.QMainWindow, Ui_Main):
         self.user_data()
 
     def create_question(self):
-        self.pitanje.setText(questions[self.i].get_question())
+        self.pitanje.setText(html.unescape(questions[self.i].get_question()))
         li = questions[self.i].all_choices()
-        self.odg1.setText(li[0])
-        self.odg2.setText(li[1])
-        self.odg3.setText(li[2])
-        self.odg4.setText(li[3])
+        self.odg1.setText(html.unescape(li[0]))
+        self.odg2.setText(html.unescape(li[1]))
+        self.odg3.setText(html.unescape(li[2]))
+        self.odg4.setText(html.unescape(li[3]))
 
     def on_next_btn(self):
-        if int(self.i) == int(len(questions)-1):
+        if int(self.i) == int(len(questions) - 1):
             self.lbl_score.setText(str(self.correct_answers))
             conn = d.create_connection()
             with conn:
@@ -58,6 +73,9 @@ class MainPage(QtWidgets.QMainWindow, Ui_Main):
                     d.update_user(conn, (self.correct_answers, a[0]))
                 except Error as e:
                     print(e)
+            self.main_page = LeadersPage()
+            self.main_page.show()
+            self.close()
         else:
             self.i += 1
             self.create_question()
